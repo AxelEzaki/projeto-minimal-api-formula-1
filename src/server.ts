4c43df7982455b9dccee22c7103d45d1b860a759
axelezaki <axelkeza@hotmail.com>
2025-08-29 18:22:36 -0300

    Adicionado mais pilotos

--- src/server.ts
+++ src/server.ts
@@ -30,9 +30,26 @@ const teams = [
 
 const drivers = [
     { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-    { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-    { id: 3, name: "Lando Norris", team: "McLaren" },
-]
+    { id: 2, name: "Sergio Pérez", team: "Red Bull Racing" },
+    { id: 3, name: "Lewis Hamilton", team: "Mercedes" },
+    { id: 4, name: "George Russell", team: "Mercedes" },
+    { id: 5, name: "Charles Leclerc", team: "Ferrari" },
+    { id: 6, name: "Carlos Sainz", team: "Ferrari" },
+    { id: 7, name: "Lando Norris", team: "McLaren" },
+    { id: 8, name: "Oscar Piastri", team: "McLaren" },
+    { id: 9, name: "Fernando Alonso", team: "Aston Martin" },
+    { id: 10, name: "Lance Stroll", team: "Aston Martin" },
+    { id: 11, name: "Pierre Gasly", team: "Alpine" },
+    { id: 12, name: "Esteban Ocon", team: "Alpine" },
+    { id: 13, name: "Alexander Albon", team: "Williams" },
+    { id: 14, name: "Logan Sargeant", team: "Williams" },
+    { id: 15, name: "Daniel Ricciardo", team: "RB Formula One Team" },
+    { id: 16, name: "Yuki Tsunoda", team: "RB Formula One Team" },
+    { id: 17, name: "Valtteri Bottas", team: "Stake F1 Team Kick Sauber" },
+    { id: 18, name: "Guanyu Zhou", team: "Stake F1 Team Kick Sauber" },
+    { id: 19, name: "Nico Hülkenberg", team: "Haas F1 Team" },
+    { id: 20, name: "Kevin Magnussen", team: "Haas F1 Team" }
+];
 
 server.get("/teams", async (req, resp) => {
     resp.type("application/json").code(200);
